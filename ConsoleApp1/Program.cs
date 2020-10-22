529faa0922b46954e13fc5039aba9544565fe9a7
Eduardo Guanco <eduardo.guanco@kdsoft.io>
2020-10-22 20:57:48 -0300

    entrenamiento de resistencia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,12 +9,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string urlOption, seleccion, password, username;
+
             Console.WriteLine("Usuario:");
-            string username = Console.ReadLine();
+            username = Console.ReadLine();
             Console.WriteLine("Contraseña:");
-            string password = Console.ReadLine();
+            password = Console.ReadLine();
             Console.WriteLine("Cantidad:");
             int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("Seleccionar:");            
+            Console.WriteLine("1 - Food:");
+            Console.WriteLine("2 - Training: resistencia");
+            seleccion = Console.ReadLine();
+
+            switch (seleccion)
+            {
+                case "1":
+                    urlOption = "https://la.footballteamgame.com/food";
+                    break;
+                case "2":
+                    urlOption = "https://la.footballteamgame.com/training";
+                    break;
+                default:
+                    return;
+            }
+
+
             IWebDriver driver = new ChromeDriver(@"C:\chromedriver");
 
             driver.Navigate().GoToUrl("https://la.footballteamgame.com");
@@ -35,18 +55,36 @@ namespace ConsoleApp1
             btnLogin.Click();
 
             Thread.Sleep(5000);
-            driver.Navigate().GoToUrl("https://la.footballteamgame.com/food");
-            
-            do
-            {
-                Thread.Sleep(5000);
-                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-                js.ExecuteScript("document.getElementsByClassName('btn btn-lg btn-primary')[0].click()");
+            driver.Navigate().GoToUrl(urlOption);
 
-                Thread.Sleep(1000 * 60 * 7);
-                driver.Navigate().Refresh();
-                count -= 1;
-            } while (count != 0);            
+            switch (seleccion)
+            {
+                case "1":
+                    do
+                    {
+                        Thread.Sleep(5000);
+                        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                        js.ExecuteScript("document.getElementsByClassName('btn btn-lg btn-primary')[0].click()");
+
+                        Thread.Sleep(1000 * 60 * 7);
+                        driver.Navigate().Refresh();
+                        count -= 1;
+                    } while (count != 0);
+                    break;
+                case "2":
+                    do
+                    {
+                        Thread.Sleep(5000);
+                        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                        js.ExecuteScript("document.getElementsByClassName('btn btn-primary tile-box__btn pulse-in-tutorial')[3].click()");
+                        Thread.Sleep(1000 * 10);
+                        driver.Navigate().Refresh();
+                        count -= 1;
+                    } while (count != 0);
+                    break;
+                default:
+                    break;
+            }       
         }
     }
 }
